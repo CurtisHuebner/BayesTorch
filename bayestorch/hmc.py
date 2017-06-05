49308c56f32fd4f1445f1b4be9ba01ddcacb8eb2
Curtis Huebner <mathcurt@gmail.com>
2017-06-04 17:12:28 -0700

    made hmc work

--- bayestorch/hmc.py
+++ bayestorch/hmc.py
@@ -1,59 +1,58 @@
 import torch as t
 import torch.nn.functional as f
-import matplotlib.pyplot as plt
 from torch.autograd import Variable
-
+from torch.optim import Optimizer
 import math
+import matplotlib.pyplot as plt
 
-class HCMCSampler:
+class HMCSampler(object):
     """updates the model parameters by preforming n hcmc updates
 
     (larger description)
     """
-    def __init__(self,model,data,nllh):
-        self.model = model
-        self.data = data
-        self.nllh = nllh
+    def __init__(self,parameters,epsilon=0.01,n=100):
+        self.epsilon = epsilon
+        self.n = n
+        self.p_list = parameters
 
     def resample_r(self):
-        p_list = self.model.parameter_list()
+        p_list = self.p_list
         self.r_list = []
         for param in p_list:
             s = param.size()
             means = t.zeros(s)
-            self.r_list.append(t.normal(means,1).cuda())#todo verify that this is correct
+            self.r_list.append(t.normal(means,1))#todo verify that this is correct
 
-    def data_pass(self):
-        p_list = self.model.parameter_list()
+    def zero_grad(self):
+        for param in self.p_list:
+            if param.grad is not None:
+                param.grad.data.zero_()
 
-        def zero_grad(x):
-            if x.grad is not None:
-                x.grad.data.zero_()
-        list(map(zero_grad,p_list))
-        output = self.model.inference(self.data)
-        loss = self.nllh(output,self.data)
+    def data_pass(self,closure):
+        p_list = self.p_list
+        self.zero_grad()
+        loss = closure()
         loss.backward()
         g_list = list(map((lambda x: x.grad.data),p_list))
         return g_list,loss
 
-    def step(self,epsilon,n=1):
+    def step(self,closure):
+        #TODO: add MC rejection
         self.resample_r()
-        p_list = [x.data for x in self.model.parameter_list()]
+        p_list = [x.data for x in self.p_list]
         r_list = self.r_list
         def assign(x,y):
             x.data = y
-        for i in range(n):
-            #TODO:Verify that this is not nessesary
-#           if (np.random.randn() > 0.5):
-#               epsilon = -epsilon
+        epsilon = self.epsilon
+        for i in range(self.n):
             #TODO: Clean up implementation with getter and setters
-            g_list,_ = self.data_pass()
+            g_list,_ = self.data_pass(closure)
             r_list = list(map(lambda x,y: x-y*epsilon/2,r_list,g_list))
 
             p_list = list(map(lambda x,y: x+y*epsilon,p_list,r_list))
-            list(map(assign,self.model.parameter_list(),p_list))
+            list(map(assign,self.p_list,p_list))
 
-            g_list,loss = self.data_pass()
+            g_list,loss = self.data_pass(closure)
             r_list = list(map(lambda x,y: x-y*epsilon/2,r_list,g_list))
 
 
@@ -88,10 +87,10 @@ class TensorParameter:
     def __init__(self,shape,std_dev,zeros = True):
         if zeros:
             self.parameter = Variable(t.FloatTensor(np.random.normal(size=shape,
-                scale=std_dev/100)).cuda(),requires_grad = True)
+                scale=std_dev/100)),requires_grad = True)
         else:
             self.parameter = Variable(t.FloatTensor(np.random.normal(size=shape,
-                scale=std_dev)).cuda(),requires_grad = True)
+                scale=std_dev)),requires_grad = True)
         self.var = std_dev*std_dev
         self.shape = shape
     
@@ -102,10 +101,10 @@ class TensorParameter:
         return self.parameter
     
     def cuda(self):
-        self.parameter.cuda()
+        self.parameter = self.parameter.cuda()
 
     def cpu(self):
-        self.parameter.cpu()
+        self.parameter = self.parameter.cpu()
 
     def get_prior_llh(self,dims):
         prob = -t.log(self.parameter)**2 \
@@ -116,12 +115,22 @@ class TensorParameter:
         return t.squeeze(prob)
 
 
-def test_hcmc():
-    pass
+def test_hmc():
+    p = Variable(t.FloatTensor([0]),requires_grad = True)
+    sampler = HMCSampler([p])
+    p_values = []
+    closure = lambda: p**2/2
+    for i in range(20000):
+        sampler.step(closure) 
+        p_values.append(scalar(p))
+    plt.hist(p_values)
+    plt.show()
+
+
 
 def scalar(x):
     return x.data.cpu().numpy()[0]
     
 
 if __name__ == "__main__":
-    main()
+    test_hmc()
